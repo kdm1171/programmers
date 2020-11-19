1d9b56c5f9f8ef78b1c5be08b28b3bc539437b14
jade <kdm1171@gmail.com>
2020-11-19 22:08:48 +0900

    정수 내림차순으로 정렬하기 문제 해결 - 문제 주소 추가.

--- src/main/java/com/jade/programmers/practice/level1/P_정수_내림차순으로_정렬하기.java
+++ src/main/java/com/jade/programmers/practice/level1/P_정수_내림차순으로_정렬하기.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.stream.IntStream;
 
 /**
+ * 문제 주소 - https://programmers.co.kr/learn/courses/30/lessons/12933
  * 문제 설명
  * 함수 solution은 정수 n을 매개변수로 입력받습니다.
  * n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
